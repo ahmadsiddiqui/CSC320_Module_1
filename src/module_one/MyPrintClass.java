7e87f87f1827d4654fe869fdfaafb0def5e05a53
Ahmad Siddiqui <ahmad.siddiqui024@gmail.com>
2023-12-17 23:13:07 -0800

    Initial commit

---
+++ src/module_one/MyPrintClass.java
@@ -0,0 +1,14 @@
+package module_one;
+
+public class MyPrintClass {
+
+	public static void main(String[] args) {
+		System.out.println("John");
+		System.out.println("Smith");
+		System.out.println("123 Java Street");
+		System.out.println("New York");
+		System.out.println("12345");
+
+	}
+
+}
